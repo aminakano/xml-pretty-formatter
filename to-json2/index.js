797b44fc5b624467ad7015ccfb128507f34f6759
Ami Nakano <ami.nakano@outlook.com>
2021-07-10 13:00:17 +0200

    generated xml

--- to-json2/index.js
+++ to-json2/index.js
@@ -31,24 +31,41 @@ fs.readFile(filePath, { encoding: "utf-8" }, (err, data) => {
 (async () => {
   try {
     const result = await xml2js.parseStringPromise(xml, {
-      mergeAttrs: true,
+      mergeAttrs: false,
     });
-    const json = await result.logicalModel.attributes[0].attribute;
+    const attr = result.logicalModel.attributes[0].attribute;
+    //  attr[0]
+    //   '$': {
+    //     id: 'AddressCorrectionKey',
+    //     order: '1',
+    //     displayAttribute: 'false',
+    //     attributeHierarchyActive: 'false'
+    //   },
+    //   descriptions: [ { '$': [Object] } ],
+    //   keyMapping: [ { '$': [Object] } ]
+    // }
+
+    // console.log(attr[0].$.id);
+    // console.log(result.logicalModel.attributes[0].attribute[0].descriptions[0]);
+    // const json = await result.logicalModel.attributes[0].attribute;
     // read properties file and convert to JSON
     fs.readFile(filePath, { encoding: "utf-8" }, (err, data) => {
       if (err) {
         throw new Error();
       } else {
         const prop = propertiesToJSON(data);
+        attr.forEach(
+          (val) => (val.descriptions[0].$.defaultDescription = prop[val.$.id])
+        );
 
-        for (let i in json) {
-          const id = json[i].id[0];
-          json[i].descriptions[0].defaultDescription[0] = prop[id] || "";
-        }
+        // for (let i in json) {
+        //   const id = json[i].id[0];
+        //   json[i].descriptions[0].defaultDescription[0] = prop[id] || "";
+        // }
         // fs.writeFileSync(`./${dateString}.json`, json);
         // console.log(json);
         // const stringified = JSON.stringify(json);
-        result.logicalModel.attributes[0].attribute = json;
+        result.logicalModel.attributes[0].attribute = attr;
         const builder = new xml2js.Builder();
         const newXml = builder.buildObject(result);
         console.log(newXml);
